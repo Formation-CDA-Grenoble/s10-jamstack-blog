0c9054624c46fcefc5a08446413b1f1d9d37dc4f
Romain Tello <romain.tello@gmail.com>
2020-03-10 15:35:35 +0100

    Make header sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,16 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { SearchForm } from '.';
 import { Link } from 'react-router-dom';
 
+const Styles = {
+  header: {
+    position: 'sticky',
+    top: 0,
+    zIndex: 1,
+  }
+}
+
 const Header = () =>
-  <header>
+  <header style={Styles.header}>
     <Navbar bg="light" expand="lg" className="mb-4">
       <Navbar.Brand>My React/DatoCMS Blog</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
